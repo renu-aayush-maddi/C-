5939ead6860dba2f30867d015b1b8179f74d2c6f
renu-aayush-maddi <2200032294@kluniversity.in>
2026-05-09 19:22:02 +0530

    Task-7 completed

---
+++ Task7/ApiService.cs
@@ -0,0 +1,23 @@
+using System.Net.Http;
+
+namespace AsyncApiDemo
+{
+    public class ApiService
+    {
+        HttpClient client = new HttpClient();
+        public async Task<string> GetUsers()
+        {
+            return await client.GetStringAsync("https://jsonplaceholder.typicode.com/users");
+        }
+
+        public async Task<string> GetPosts()
+        {
+            return await client.GetStringAsync("https://jsonplaceholder.typicode.com/posts");
+        }
+
+        public async Task<string> GetComments()
+        {
+            return await client.GetStringAsync("https://jsonplaceholder.typicode.com/comments");
+        }
+    }
+}
---
+++ Task7/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AsyncApiDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ApiService service = new ApiService();
+
+            try
+            {
+                Console.WriteLine("Fetching data...\n");
+
+                Task<string> usersTask = service.GetUsers();
+                Task<string> postsTask = service.GetPosts();
+                Task<string> commentsTask = service.GetComments();
+
+                await Task.WhenAll(usersTask,postsTask,commentsTask);
+
+                Console.WriteLine("Users Data Length: " + usersTask.Result.Length);
+                Console.WriteLine("Posts Data Length: " + postsTask.Result.Length);
+                Console.WriteLine("Comments Data Length: " + commentsTask.Result.Length);
+                Console.WriteLine("\nAll API calls completed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
